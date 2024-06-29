4f0fc573350665c2375c0186e7495b6d57697704
gideonjon <gideonjones63@gmail.com>
2024-06-29 21:00:08 +0100

    added-now

--- app/src/main/java/com/dash/rigour/data/JobsInfo.kt
+++ app/src/main/java/com/dash/rigour/data/JobsInfo.kt
@@ -1,5 +1,8 @@
 package com.dash.rigour.data
 
 data class JobsInfo(
-
+    var projectType: String? = null,
+    var description: String? = null,
+    var projectTitle: String? = null,
+    var workersNeeded: String? = null
 )
--- app/src/main/java/com/dash/rigour/fragment/AddProjects.kt
+++ app/src/main/java/com/dash/rigour/fragment/AddProjects.kt
@@ -6,16 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dash.rigour.R
+import com.dash.rigour.databinding.FragmentAddProjectsBinding
 
 
 class AddProjects : Fragment() {
+    private var _binding: FragmentAddProjectsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_projects, container, false)
+
+        _binding = FragmentAddProjectsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+if
+
+
+        return view
     }
 
 
